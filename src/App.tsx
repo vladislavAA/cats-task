d2e49bb8a91b3e9c6b3b214a95ef7dd533939416
vladislavAA <aryanov.vladislav@gmail.com>
2024-01-31 09:24:57 +0300

    cats-task

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import AppRouter from "./pages";
+
+import "./App.scss";
+
+const App: FC = () => {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
